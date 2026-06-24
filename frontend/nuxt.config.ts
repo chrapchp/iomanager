e2b925bc80ab19bbffc776e4c45c4bb5195eed95
Peter Chrapchynski <chrapchp@cogitomethods.com>
2026-06-24 16:56:11 -0600

    disable path-prefixing in nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@
  * Author:      Peter Chrapchynski
  * Date:        2026Jun23
  * History:     2026Jun23 - Initial creation
+ *              2026Jun24 - Disable component path-prefix so subdirs are organisational only
  ***************************************************/
 
 import tailwindcss from '@tailwindcss/vite'
@@ -11,6 +12,9 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   modules: ['@pinia/nuxt'],
+  components: {
+    dirs: [{ path: '~/components', pathPrefix: false }],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
